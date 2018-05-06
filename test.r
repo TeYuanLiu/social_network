6b0fefdede9570d00b1bbddf61d5e3809e82b9d6
TeYuanLiu <teyuanliu@g.ucla.edu>
2018-05-06 00:09:50 -0700

    dispersion computation success

--- test.r
+++ test.r
@@ -14,32 +14,42 @@ plot_dispersion_embeddedness = function(g, core_id){
     p = induced.subgraph(g, c(which(V(g)$id==core_id), neighbors(g, which(V(g)$id==core_id))))
     V(p)$embeddedness = rep(-1, vcount(p))
     V(p)$dispersion = rep(-1, vcount(p))
+    
     for(i in V(p)$id){
         if(i != core_id){
+            print(i)
             mn_id_vec = intersect(neighbors(p, which(V(p)$id==i))$id, neighbors(p, which(V(p)$id==core_id))$id)
-            V(p)[which(V(p)$id==i)]$embeddedness = length(mn_id_vec)
-            p_del = delete.vertices(p, c(which(V(p)$id==i), which(V(p)$id==core_id)))
-            dist_sum = 0
-            if(length(mn_id_vec)==1){
-                dist_sum = 0  
+            if(length(mn_id_vec) == 0){
+                V(p)[which(V(p)$id==i)]$embeddedness = 0
+                V(p)[which(V(p)$id==i)]$dispersion = 0
             }
             else{
-                for(j in 1:(length(mn_id_vec)-1)){
-                    v_j = mn_id_vec[j]
-                    for(k in (j+1):length(mn_id_vec)){
-                        v_k = mn_id_vec[k]
-                        dist = shortest.paths(p_del, mode="all")[which(V(p_del)$id==v_j), which(V(p_del)$id==v_k)]
-                        if(dist == Inf){
-                            dist_sum = dist_sum + diameter(p)
-                        }
-                        else{
-                            dist_sum = dist_sum + dist
+                V(p)[which(V(p)$id==i)]$embeddedness = length(mn_id_vec)
+                p_del = delete.vertices(p, c(which(V(p)$id==i), which(V(p)$id==core_id)))
+                dist_sum = 0
+                if(length(mn_id_vec)==1){
+                    dist_sum = 0  
+                }
+                else{
+                    dist_all = shortest.paths(p_del, mode="all")
+                    for(j in 1:(length(mn_id_vec)-1)){
+                        v_j = mn_id_vec[j]
+                        for(k in (j+1):length(mn_id_vec)){
+                            v_k = mn_id_vec[k]
+                            dist = dist_all[which(V(p_del)$id==v_j), which(V(p_del)$id==v_k)]
+                            if(dist == Inf){
+                                dist_sum = dist_sum + diameter(p)
+                            }
+                            else{
+                                dist_sum = dist_sum + dist
+                            }
                         }
                     }
-                }
 
+                }
+                V(p)[which(V(p)$id==i)]$dispersion = dist_sum
             }
-            V(p)[which(V(p)$id==i)]$dispersion = dist_sum
+
         }
     }
     id_max_disp = V(p)[which.max(V(p)$dispersion)]$id
@@ -51,8 +61,8 @@ plot_dispersion_embeddedness = function(g, core_id){
 # Main Function
 ##############################
 main = function(){
-    g = read.graph("test.txt", format="edgelist", directed=FALSE)
+    g = read.graph("facebook_combined.txt", format="edgelist", directed=FALSE)
     V(g)$id = 1:vcount(g)
-    plot_dispersion_embeddedness(g, 6)
+    plot_dispersion_embeddedness(g, 1)
 }
 main()
